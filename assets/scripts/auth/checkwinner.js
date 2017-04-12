ed5997b97d00943d8b4248ad67b35d4be9448878
yourUsername <joseph.belmonte@gmail.com>
2017-04-11 22:52:45 -0400

    Addition game engine work - Xs and Os appear
    Added functionality for Xs and Os to appear on click.
    You cannot click into a square more than once in the same game.
    The start new game button will reset all squares to be empty.

--- assets/scripts/auth/checkwinner.js
+++ assets/scripts/auth/checkwinner.js
@@ -1,10 +1,18 @@
 // code to check if there is a winner
+// const fillBoard = function (array) {
+//   for (let i = 0; i === 8; i++) {
+//     if (gameBoard[i] === 0) {
+//       gameBoard[i] = 5
+//     }
+//   }
+// }
 
 const checkWinner = function (gameBoard) {
   console.log('Checking for a winner using game board: ', gameBoard)
   const topRow = gameBoard[0] + gameBoard[1] + gameBoard[2]
   if (topRow === 3) {
     console.log('Player 1 is the winner!')
+    // fillBoard(gameBoard)
   }
   if (topRow === -3) {
     console.log('Player 2 is the winner!')
--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -4,13 +4,23 @@ const api = require('./api')
 const ui = require('./ui')
 const checkwinner = require('./checkwinner')
 
-let gameBoard = []
+let gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 let activePlayer = 1
 
 const startGame = function () {
+  event.preventDefault()
   console.log('start game function initialized!')
   gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0]
   activePlayer = 1
+  document.getElementsByClassName('zero')[0].innerHTML = ''
+  document.getElementsByClassName('one')[0].innerHTML = ''
+  document.getElementsByClassName('two')[0].innerHTML = ''
+  document.getElementsByClassName('three')[0].innerHTML = ''
+  document.getElementsByClassName('four')[0].innerHTML = ''
+  document.getElementsByClassName('five')[0].innerHTML = ''
+  document.getElementsByClassName('six')[0].innerHTML = ''
+  document.getElementsByClassName('seven')[0].innerHTML = ''
+  document.getElementsByClassName('eight')[0].innerHTML = ''
   console.log(gameBoard)
   console.log(activePlayer)
 }
@@ -63,63 +73,129 @@ const signOut = function (event) {
 const gamePlay0 = function () {
   console.log('Calling gamePlay 0 function')
   console.log('The current active player is: ', activePlayer)
+  console.log('The game board here is: ', gameBoard)
+  if (gameBoard[0] === 0) {
+    if (activePlayer === 1) {
+      document.getElementsByClassName('zero')[0].innerHTML = 'X'
+    } else {
+      document.getElementsByClassName('zero')[0].innerHTML = 'O'
+    }
+  }
   gamePlay(0)
 }
 
 const gamePlay1 = function () {
   console.log('Calling gamePlay 1 function')
   console.log('The current active player is: ', activePlayer)
+  if (gameBoard[1] === 0) {
+    if (activePlayer === 1) {
+      document.getElementsByClassName('one')[0].innerHTML = 'X'
+    } else {
+      document.getElementsByClassName('one')[0].innerHTML = 'O'
+    }
+  }
   gamePlay(1)
 }
 
 const gamePlay2 = function () {
   console.log('Calling gamePlay 2 function')
   console.log('The current active player is: ', activePlayer)
+  if (gameBoard[2] === 0) {
+    if (activePlayer === 1) {
+      document.getElementsByClassName('two')[0].innerHTML = 'X'
+    } else {
+      document.getElementsByClassName('two')[0].innerHTML = 'O'
+    }
+  }
   gamePlay(2)
 }
 
 const gamePlay3 = function () {
   console.log('Calling gamePlay 3 function')
   console.log('The current active player is: ', activePlayer)
+  if (gameBoard[3] === 0) {
+    if (activePlayer === 1) {
+      document.getElementsByClassName('three')[0].innerHTML = 'X'
+    } else {
+      document.getElementsByClassName('three')[0].innerHTML = 'O'
+    }
+  }
   gamePlay(3)
 }
 
 const gamePlay4 = function () {
   console.log('Calling gamePlay 4 function')
   console.log('The current active player is: ', activePlayer)
+  if (gameBoard[4] === 0) {
+    if (activePlayer === 1) {
+      document.getElementsByClassName('four')[0].innerHTML = 'X'
+    } else {
+      document.getElementsByClassName('four')[0].innerHTML = 'O'
+    }
+  }
   gamePlay(4)
 }
 
 const gamePlay5 = function () {
   console.log('Calling gamePlay 5 function')
   console.log('The current active player is: ', activePlayer)
+  if (gameBoard[5] === 0) {
+    if (activePlayer === 1) {
+      document.getElementsByClassName('five')[0].innerHTML = 'X'
+    } else {
+      document.getElementsByClassName('five')[0].innerHTML = 'O'
+    }
+  }
   gamePlay(5)
 }
 
 const gamePlay6 = function () {
   console.log('Calling gamePlay 6 function')
   console.log('The current active player is: ', activePlayer)
+  if (gameBoard[6] === 0) {
+    if (activePlayer === 1) {
+      document.getElementsByClassName('six')[0].innerHTML = 'X'
+    } else {
+      document.getElementsByClassName('six')[0].innerHTML = 'O'
+    }
+  }
   gamePlay(6)
 }
 
 const gamePlay7 = function () {
   console.log('Calling gamePlay 7 function')
   console.log('The current active player is: ', activePlayer)
+  if (gameBoard[7] === 0) {
+    if (activePlayer === 1) {
+      document.getElementsByClassName('seven')[0].innerHTML = 'X'
+    } else {
+      document.getElementsByClassName('seven')[0].innerHTML = 'O'
+    }
+  }
   gamePlay(7)
 }
 
 const gamePlay8 = function () {
   console.log('Calling gamePlay 8 function')
   console.log('The current active player is: ', activePlayer)
+  if (gameBoard[8] === 0) {
+    if (activePlayer === 1) {
+      document.getElementsByClassName('eight')[0].innerHTML = 'X'
+    } else {
+      document.getElementsByClassName('eight')[0].innerHTML = 'O'
+    }
+  }
   gamePlay(8)
 }
 
 const gamePlay = function (square) {
   // event.preventDefault()
   console.log('Game play was initialized and the event is: ', event)
+  console.log('Game board square 0 value is: ', gameBoard[square])
   if (gameBoard[square] === 0) {
     if (activePlayer === 1) {
       gameBoard[square] = 1
+
     } else {
       gameBoard[square] = -1
     }
